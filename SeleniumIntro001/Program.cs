4a1fd5190c6316be5412489faa7b59ce8b73f08f
thierry le gall <thierry28220@live.fr>
2021-09-16 10:28:23 +0200

    Ajoutez des fichiers projet.

---
+++ SeleniumIntro001/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace SeleniumIntro001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(@"c:\Drivers\web");
+            driver.Navigate().GoToUrl("https://www.google.fr/");
+            var element = driver.FindElement(By.XPath("//*[@id=\"L2AGLb\"]"));
+            Thread.Sleep(5000);
+            element.Click();
+            element = driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+            element.SendKeys("webshop");
+            element.Submit();
+            
+        }
+    }
+}
